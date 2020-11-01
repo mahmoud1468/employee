fea7f41404d3afe192533b85cbc174d24d9a7964
Mojtaba Varmazyar <mahmoud1468@gmail.com>
2020-11-01 13:15:05 +0330

    Use @ParametrizedTest to simplify and clean test cases.

--- src/test/java/junit/tutorial/employee/util/StringUtilTest.java
+++ src/test/java/junit/tutorial/employee/util/StringUtilTest.java
@@ -1,36 +1,39 @@
 package junit.tutorial.employee.util;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static junit.tutorial.employee.util.StringUtil.isName;
 import static junit.tutorial.employee.util.StringUtil.isNationalId;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringUtilTest {
 
-    @Test
-    void isName_WHEN_nameIsValid_THEN_returnsTrue() {
-        assertTrue(isName("ali"));
-        assertTrue(isName("fatemeh-eskandari"));
-        assertTrue(isName("fatemeh eskandari"));
-        assertTrue(isName("ali mohammad-reza"));
-        assertTrue(isName("ali-mohammad reza"));
-        assertTrue(isName("ali mohammad reza"));
-        assertTrue(isName("qwertyuioplkjhgfdsazxcvbnm"));
+    @Tag("parametrized")
+    @ParameterizedTest
+    @ValueSource(strings = {"ali", "fatemeh-eskandari", "ali mohammad-reza", "qwertyuioplkjhgfdsazxcvbnm"})
+    void isName_WHEN_nameIsValid_THEN_returnsTrue(String parameter) {
+        assertTrue(isName(parameter));
     }
 
-    @Test
-    void isName_WHEN_nameIsNotValid_THEN_returnsFalse(){
-        assertFalse(isName(null));
-        assertFalse(isName(""));
-        assertFalse(isName("ali1"));
-        assertFalse(isName("maryam "));
-        assertFalse(isName("-marzieh"));
-        assertFalse(isName("ali  reza"));
-        assertFalse(isName("Alireza"));
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"maryam ", "-marzieh", "ali  reza", "Alirza"})
+    void isName_WHEN_nameIsNotValid_THEN_returnsFalse(String parameter){
+        assertFalse(isName(parameter));
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("populateValidNationalIds")
+    @Disabled
     void isNationalId_WHEN_nationalIdIsValid_THEN_returnsTrue() {
         assertTrue(isNationalId("5300020788"));
         assertTrue(isNationalId("0520258649"));
@@ -38,7 +41,12 @@ class StringUtilTest {
         assertTrue(isNationalId("0017440701"));
     }
 
+    static Stream<String> populateValidNationalIds(){
+        return Stream.of("5300020788","0520258649");
+    }
+
     @Test
+    @Disabled
     void isNationalId_WHEN_nationalIdIsNotValid_THEN_returnsFalse() {
         assertFalse(isNationalId(null));
         assertFalse(isNationalId(""));
