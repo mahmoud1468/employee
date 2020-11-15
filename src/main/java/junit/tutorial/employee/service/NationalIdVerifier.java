47f8bd855da10204cee268bd800c77a86522c31f
Mojtaba Varmazyar <mahmoud1468@gmail.com>
2020-11-15 16:52:54 +0330

    Fix a typo.

--- src/main/java/junit/tutorial/employee/service/NationalIdVerifier.java
+++ src/main/java/junit/tutorial/employee/service/NationalIdVerifier.java
@@ -11,7 +11,7 @@ public interface NationalIdVerifier {
      * @param nationalId: the national id to be verified.
      * @return personal information (first name, last name, birth date) of the person owns this national id.
      * @throws ServiceUnavailableException : in case is not able to connect to the Government service
-     * @throws IllegalArgumentException : in case input is not well-formed (null, or any string other than 10 digits
+     * @throws IllegalArgumentException : in case input is not well-formed (null, or any string other than 10 digits)
      * @throws junit.tutorial.employee.exception.IllegalNationalIdException : in case national id is not owned by anyone.
      */
     PersonDTO verify(String nationalId) throws ServiceUnavailableException;
